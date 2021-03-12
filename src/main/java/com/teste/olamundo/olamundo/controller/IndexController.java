58ed87e0e54a2c97cc5aa412ee9bec3bd83f6669
thiagomeloo <4thiagomelo5@gmail.com>
2021-03-12 02:57:33 -0300

    add dependencia thymeleaf

---
+++ src/main/java/com/teste/olamundo/olamundo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.teste.olamundo.olamundo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+  
+  @RequestMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+}
+
+
+
